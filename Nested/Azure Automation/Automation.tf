f60fe2ab5d0a5a1942a02f3ab5421ece82539af6
azjon <50624288+azjon@users.noreply.github.com>
2020-02-17 14:50:31 +0000

    Update Azure automation

--- Nested/Azure Automation/Automation.tf
+++ Nested/Azure Automation/Automation.tf
@@ -2,15 +2,15 @@ provider "azurerm" {
   version = "~> 1.33.1"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "resourceGroup1"
+resource "azurerm_resource_group" "automation" {
+  name     = "tf-rg-core-aut"
   location = "West Europe"
 }
 
-resource "azurerm_automation_account" "example" {
-  name                = "automationAccount1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_automation_account" "automation" {
+  name                = "tf-aa-core-aut"
+  location            = azurerm_resource_group.automation.location
+  resource_group_name = azurerm_resource_group.automation.name
 
   sku_name = "Basic"
 
