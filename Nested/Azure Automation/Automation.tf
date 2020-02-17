2b14a60a21a26e7da9a2a2b879639f6f5b827864
azjon <50624288+azjon@users.noreply.github.com>
2020-02-17 17:06:00 +0000

    Add update management

--- Nested/Azure Automation/Automation.tf
+++ Nested/Azure Automation/Automation.tf
@@ -37,3 +37,117 @@ resource "azurerm_automation_module" "xActiveDirectory" {
     uri = "https://devopsgallerystorage.blob.core.windows.net/packages/xactivedirectory.2.19.0.nupkg"
   }
 }
+
+resource "azurerm_automation_softwareUpdateConfigurations" "windows_batch" {
+  location            = "West Europe"
+  name                = "windows_batch"
+  resource_group_name = azurerm_resource_group.automation.name
+
+  update_configuration = {
+    operating_system = "Windows"
+
+    windows = {
+      included_update_classifications = ""
+      excludedKbNumbers               = ""
+      reboot_setting                  = "IfRequired"
+    }
+    duration               = "120"
+    azure_virtual_machines = []
+
+    targets = {
+      scope     = []
+      locations = []
+
+      tagSettings = {
+        tags           = "UpdateGroup"
+        filterOperator = "UpdateGroup01"
+      }
+    }
+    scheduleInfo = {
+      startTime = "07:00"
+      expiryTime = ""
+      expiryTimeOffsetMinutes ="",
+      isEnabled =  true,
+      nextRun = "string"
+      nextRunOffsetMinutes= "number",
+      interval= "integer",
+      frequency= "string",
+      timeZone= "string",
+      advancedSchedule= {
+        monthlyOccurrences= [
+          {
+            occurrence = "1"
+            day = "tuesday"
+          }
+        ]
+      }
+    }
+    tasks= {
+      preTask = {
+        parameters = "optional runbook parameters"
+        source = "runbookname"
+      }
+      postTask = {
+        parameters = "optional runbook parameters"
+        source = "runbookname"
+      }
+    }
+  }
+}
+
+resource "azurerm_automation_softwareUpdateConfigurations" "linux_batch" {
+  location            = "West Europe"
+  name                = "linux_batch"
+  resource_group_name = azurerm_resource_group.automation.name
+
+  update_configuration = {
+    operating_system = "Linux"
+
+    windows = {
+      included_update_classifications = ""
+      excludedKbNumbers               = ""
+      reboot_setting                  = "IfRequired"
+    }
+    duration               = "120"
+    azure_virtual_machines = []
+
+    targets = {
+      scope     = []
+      locations = []
+
+      tagSettings = {
+        tags           = "UpdateGroup"
+        filterOperator = "UpdateGroup01"
+      }
+    }
+    scheduleInfo = {
+      startTime = "07:00"
+      expiryTime = ""
+      expiryTimeOffsetMinutes ="",
+      isEnabled =  true,
+      nextRun = "string"
+      nextRunOffsetMinutes= "number",
+      interval= "integer",
+      frequency= "string",
+      timeZone= "string",
+      advancedSchedule= {
+        monthlyOccurrences= [
+          {
+            occurrence = "1"
+            day = "tuesday"
+          }
+        ]
+      }
+    }
+    tasks= {
+      preTask = {
+        parameters = "optional runbook parameters"
+        source = "runbookname"
+      }
+      postTask = {
+        parameters = "optional runbook parameters"
+        source = "runbookname"
+      }
+    }
+  }
+}
